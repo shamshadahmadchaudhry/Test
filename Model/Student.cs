e03dd9e5a1bfd27dc722365c6e2de4894b08faa8
Shamshad Ahmad <shamshad.ahmad@stits.co>
2021-05-18 19:03:12 +0530

    init

---
+++ Model/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DemoTest.Model
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Emailid { get; set; }
+        public string Contactno { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Model/TESTDBContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DemoTest.Model
+{
+    public partial class TESTDBContext : DbContext
+    {
+        public TESTDBContext()
+        {
+        }
+
+        public TESTDBContext(DbContextOptions<TESTDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-836DU4E\\;Database=TESTDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("STUDENT");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("ADDRESS");
+
+                entity.Property(e => e.Code)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("CODE");
+
+                entity.Property(e => e.Contactno)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("CONTACTNO");
+
+                entity.Property(e => e.Emailid)
+                    .HasMaxLength(40)
+                    .IsUnicode(false)
+                    .HasColumnName("EMAILID");
+
+                entity.Property(e => e.Firstname)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("FIRSTNAME");
+
+                entity.Property(e => e.Lastname)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("LASTNAME");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
